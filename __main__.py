8659dddb74fc979509a65a1224a28774d1e4a7da
Brandon Woo <bmw.woo@gmail.com>
2016-12-06 16:30:00 -0800

    Minor edits

--- __main__.py
+++ __main__.py
@@ -2,22 +2,6 @@ import json
 import SECRET
 import requests
 
-data = {
-   'name' : 'ACME',
-   'shares' : 100,
-   'price' : 542.23
-}
-
-arr = [data, data]
-
-# Writing JSON data
-with open('dataDump.txt', 'w') as f:
-    json.dump(arr,  f)
-
-# # Reading data back
-with open('dataDump.txt', 'r') as f:
-     test = json.load(f)
-
 param = "onion+soup"
 
 url = "http://api.yummly.com/v1/api/recipes?_app_id=%s&_app_key=%s&q=%s" % (SECRET.APP_ID, SECRET.APP_KEY, param)
